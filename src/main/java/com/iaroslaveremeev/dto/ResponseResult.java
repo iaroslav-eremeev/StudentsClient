d123e415e8dc51c5804b843d0b7bb55ac6fd2344
Iaroslav <eremeev.pt@gmail.com>
2023-01-31 22:30:43 +0200

    Initial commit

---
+++ src/main/java/com/iaroslaveremeev/dto/ResponseResult.java
@@ -0,0 +1,44 @@
+package com.iaroslaveremeev.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ResponseResult<T> {
+    private boolean result;
+    private String message;
+    private T object;
+
+    public ResponseResult(boolean result, String message, T object) {
+        this.result = result;
+        this.message = message;
+        this.object = object;
+    }
+
+    public ResponseResult() {
+    }
+
+    public boolean isResult() {
+        return result;
+    }
+
+    public void setResult(boolean result) {
+        this.result = result;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getObject() {
+        return object;
+    }
+
+    public void setObject(T object) {
+        this.object = object;
+    }
+
+}
